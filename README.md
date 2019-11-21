bd8d511b4a6cc986c119d4ac7e61ca7e95aab3fd
RyosukeCla <ryosukeclarinet@gmail.com>
2019-11-21 17:33:08 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # kustomize-plugins
+
+## sealed secret transformer
+
+### prerequisites
+
+- go 1.13.4
+- kustomize 3.4.0
+- sealed secret 0.9.5
+
+### installation
+
+```
+$ make setup
+```
+
+### test
+
+- unit test: `make unit-test`
+- test: `make test`
--- scripts/install_kustomize.sh
+++ scripts/install_kustomize.sh
@@ -4,11 +4,18 @@ set -eux
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 ROOT_DIR=$CURRENT_DIR/..
 BIN_DIR=$ROOT_DIR/bin
+VENDOR_DIR=$ROOT_DIR/vendor
+
+rm -rf ${BIN_DIR} ${VENDOR_DIR} || true
 
 mkdir -p ${BIN_DIR}
+mkdir -p ${VENDOR_DIR}
 
-# install kustomize
-curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.4.0/kustomize_v3.4.0_darwin_amd64.tar.gz -o ${BIN_DIR}/kustomize.tar.gz
-tar zxvf ${BIN_DIR}/kustomize.tar.gz -C ${BIN_DIR}
-rm ${BIN_DIR}/kustomize.tar.gz
-chmod +x ${BIN_DIR}/kustomize
+
+# get kustomize 3.4.0
+curl -L https://github.com/kubernetes-sigs/kustomize/archive/kustomize/v3.4.0.tar.gz -o ${VENDOR_DIR}/kustomize.tar.gz
+tar zxvf ${VENDOR_DIR}/kustomize.tar.gz -C ${VENDOR_DIR}
+rm ${VENDOR_DIR}/kustomize.tar.gz
+
+# build
+cd ${VENDOR_DIR}/kustomize-kustomize-v3.4.0/kustomize; go build -o ${BIN_DIR}/kustomize
